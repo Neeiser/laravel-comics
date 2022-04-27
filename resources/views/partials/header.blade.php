c7f513562aa25fdeab71617fbc7e2f34d4bd5d37
Neeiser <stortini.study@gmail.com>
2022-04-27 16:49:23 +0200

    page margin + header's styles

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,27 +1,31 @@
 <header>
 
-    <div class="header-trademarks">
-        <div>DC POWER <span>&#8480;</span> VISA <span>&reg;</span></div>
-        <div>Additional DC Sites</div>
+    <div class="header-trademarks-container">
+        <div class="margin-100 header-trademarks-box">
+            <div>DC POWER <span>&#8480;</span> VISA <span>&reg;</span></div>
+            <div>Additional DC Sites</div>
+        </div>
     </div>
 
     <div class="navbar-container">
-        <img src="./images/dc-logo.png" alt="">
-        <nav>
-            <ul>
-                <li>characters</li>
-                <li>comics</li>
-                <li>movies</li>
-                <li>tv</li>
-                <li>games</li>
-                <li>collectibles</li>
-                <li>videos</li>
-                <li>fans</li>
-                <li>news</li>
-                <li>shop</li>
-            </ul>
-        </nav>
-        <input type="text">
+        <div class="margin-100 navbar-box">
+            <img src="./images/dc-logo.png" alt="">
+            <nav>
+                <ul>
+                    <li>characters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </nav>
+            <input type="text">
+        </div>
     </div>
 
 </header>
