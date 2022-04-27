6cc7726df992fc6a481ee59641e3c970a1db16e6
Neeiser <stortini.study@gmail.com>
2022-04-27 17:54:05 +0200

    footer list done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,55 @@
 <footer>
     <div class="footer-container">
-        <div class="margin-100">ciao</div>
+        <div class="margin-100 footer-box">
+            <div class="footer-lists">
+                <div>
+                    <h2>dc comics</h2>
+                    <ul>
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>dc</h2>
+                    <ul>
+                        <li>Terms Of Use</li>
+                        <li>Privacy Policy (New)</li>
+                        <li>Ad Choise</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshop</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Helps</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>sites</h2>
+                    <ul>
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>shop</h2>
+                    <ul>
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                <span>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="">noted here</a>. All rights reserved.<br><a href="">Cookies Settings</a></span>
+            </div>
+            <div class="footer-logo"></div>
+        </div>
     </div>
 </footer>
