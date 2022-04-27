5c9b55ea0be322f79eccf9af36a68b0ceab905bf
Neeiser <stortini.study@gmail.com>
2022-04-27 15:14:58 +0200

    structuring

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+
+    <div>
+        <div>DC POWER <span>&#8480;</span> VISA <span>&reg;</span></div>
+        <div>Additional DC Sites</div>
+    </div>
+
+    <div>
+        <img src="" alt="">
+        <nav>
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </nav>
+        <input type="text">
+    </div>
+</header>
