a8e7a0cf9a3b32e5c0da0f03eb1ed2008c407c57
Neeiser <stortini.study@gmail.com>
2022-04-29 00:22:55 +0200

    exercise done

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -12,7 +12,32 @@
                     <h2>{{ $comic['series'] }}</h2>
                 </div>
             @endforeach
+            <button>LOAD MORE</button>
         </div>
+        <section>
+            <div class="margin-100 merch-container">
+                <a href="">
+                    <img src="./images/buy-comics-digital-comics.png" alt="">
+                    <span>DIGITAL COMICS</span>
+                </a>
+                <a href="">
+                    <img src="./images/buy-comics-merchandise.png" alt="">
+                    <span>DC MERCHANDISE</span>
+                </a>
+                <a href="">
+                    <img src="./images/buy-comics-subscriptions.png" alt="">
+                    <span>SUBSCRIPTION</span>
+                </a>
+                <a href="">
+                    <img src="./images/buy-comics-shop-locator.png" alt="">
+                    <span>COMIC SHOP LOCATOR</span>
+                </a>
+                <a href="">
+                    <img src="./images/buy-dc-power-visa.svg" alt="">
+                    <span>DC POWER VISA</span>
+                </a>
+            </div>
+        </section>
     </main>
 @endsection
     
